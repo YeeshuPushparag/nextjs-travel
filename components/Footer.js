bccfd1d09a55a066c6dfd3817125b553f652afe2
YeeshuPushparag <pushparagyeeshu@gmail.com>
2023-01-13 03:20:02 -0800

    linked to my profile

--- components/Footer.js
+++ components/Footer.js
@@ -1,67 +1,102 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer>
-        <div className="footer-container">
+      <div className="footer-container">
         <div className="footer-about">
-            <h2>About Us</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, rem! Nulla excepturi ullam dolor omnis iste quae autem modi numquam.</p>
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            rem! Nulla excepturi ullam dolor omnis iste quae autem modi numquam.
+          </p>
         </div>
         <div className="footer-location">
-            <h2>Our Locations</h2>
-            <ul>
-                <li>USA</li>
-                <li>Italy</li>
-                <li>Australia</li>
-                <li>Germany</li>
-            </ul>
+          <h2>Our Locations</h2>
+          <ul>
+            <li>
+              <a href="/#" className="link">
+                USA
+              </a>
+            </li>
+            <li>
+              <a href="/" className="link">
+                Italy
+              </a>
+            </li>
+            <li>
+              <a href="/" className="link">
+                Australia
+              </a>
+            </li>
+            <li>
+              <a href="/" className="link">
+                Germany
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="footer-links">
-        <h2>Quick Links</h2>
-        <ul>
-        <li>
-          <Link href="/" className="link">
-            Home
-          </Link>
-        </li>
-        <li>
-          <a href="/#packages" className="link">
-            Packages
-          </a>
-        </li>
-        <li>
-          <a href="/#services" className="link">
-            Services
-          </a>
-        </li>
-        <li>
-          <a href="/#gallery" className="link">
-            Gallery
-          </a>
-        </li> 
-        <li>
-          <a href="/#contact" className="link">
-            Contact
-          </a>
-        </li>
-      </ul>
+          <h2>Quick Links</h2>
+          <ul>
+            <li>
+              <Link href="/" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <a href="/#packages" className="link">
+                Packages
+              </a>
+            </li>
+            <li>
+              <a href="/#services" className="link">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="/#gallery" className="link">
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href="/#contact" className="link">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="footer-social-media">
-            <h2>Follow Us</h2>
-            <ul>
-                <li>Facebook</li>
-                <li>Instagram</li>
-                <li>Twitter</li>
-                <li>Linkdin</li>
-            </ul>
+          <h2>Follow Us</h2>
+          <ul>
+            <li>
+              <a href="/#" className="link">
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a href="/" className="link">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="/" className="link">
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a href="/" className="link">
+                LinkdIn
+              </a>
+            </li>
+          </ul>
         </div>
-        </div>
-        <div className="footer-copyright">
-            <p>Copyright &copy; Created by Yeeshu Pushparag</p>
-        </div>
+      </div>
+      <div className="footer-copyright">
+        <p>Copyright &copy; Created by <a href="https://yeeshupushparag.github.io/" className="link creator"  target='_blank' >Yeeshu Pushparag</a></p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
